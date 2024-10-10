246d6df13b474aafde2d9c1198bae9b1c74058df
Fapse <fb-online@posteo.de>
2024-10-10 23:28:33 +0200

    Convert to smoothed grayscale image.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mandelbrot import MandelbrotSet
-mandelbrot_set = MandelbrotSet(max_iterations=20)
+mandelbrot_set = MandelbrotSet(max_iterations=20, escape_radius=1000)
 
 width, height = 512, 512
 scale = 0.0075
@@ -10,7 +10,7 @@ image = Image.new(mode=GRAYSCALE, size=(width, height))
 for y in range(height):
     for x in range(width):
         c = scale * complex(x - width / 2, height / 2 - y)
-        instability = 1 - mandelbrot_set.stability(c)
+        instability = 1 - mandelbrot_set.stability(c, smooth=True)
         image.putpixel((x, y), int(instability * 255))
 
 image.show()
