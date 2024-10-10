2974a496280e58718798bf0436326c1cb7613ac9
Fapse <fb-online@posteo.de>
2024-10-10 22:35:05 +0200

    Make use of data class.

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from mandelbrot import MandelbrotSet
+mandelbrot_set = MandelbrotSet(max_iterations=20)
+
+width, height = 512, 512
+scale = 0.0075
+BLACK_AND_WHITE = "1"
+
 from PIL import Image
+image = Image.new(mode=BLACK_AND_WHITE, size=(width, height))
+for y in range(height):
+    for x in range(width):
+        c = scale * complex(x - width / 2, height / 2 - y)
+        image.putpixel((x, y), c not in mandelbrot_set)
 
-#np.warnings.filterwarnings("ignore")
-
-def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
-    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
-    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
-
-def is_stable(c, num_iterations):
-    z = 0
-    for _ in range(num_iterations):
-        z = z ** 2 + c
-    return abs(z) <= 2
-
-def get_members(c, num_iterations):
-    mask = is_stable(c, num_iterations)
-    return c[mask]
-
-if __name__ == '__main__':
-    c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=4096)
-    image = Image.fromarray(~is_stable(c, num_iterations=20))
-    image.show()
+image.show()
