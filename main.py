8d7f32c825684bf2f0f08585b471d673e54973cc
Fapse <fb-online@posteo.de>
2024-10-11 09:53:06 +0200

    Introduce viewport class for better panning and zooming.

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 # Based on the course on the RealPython website:
 # https://realpython.com/mandelbrot-set-python/
 
-from mandelbrot import MandelbrotSet
-mandelbrot_set = MandelbrotSet(max_iterations=20, escape_radius=1000)
-
-width, height = 512, 512
-scale = 0.0075
-GRAYSCALE = "L"
-
 from PIL import Image
-image = Image.new(mode=GRAYSCALE, size=(width, height))
-for y in range(height):
-    for x in range(width):
-        c = scale * complex(x - width / 2, height / 2 - y)
-        instability = 1 - mandelbrot_set.stability(c, smooth=True)
-        image.putpixel((x, y), int(instability * 255))
+from PIL import ImageEnhance
+from mandelbrot import MandelbrotSet
+from viewport import Viewport
 
-image.show()
+mandelbrot_set = MandelbrotSet(max_iterations=256, escape_radius=1000)
+
+image = Image.new(mode="L", size=(512, 512))
+
+for pixel in Viewport(image, center=-0.7435 + 0.1314j, width=0.002):
+    c = complex(pixel)
+    instability = 1 - mandelbrot_set.stability(c, smooth=True)
+    pixel.color = int(instability * 255)
+
+enhancer = ImageEnhance.Brightness(image)
+enhancer.enhance(1.25).show()
