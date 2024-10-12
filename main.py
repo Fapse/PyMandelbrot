a7126de5c0bafd745a54cd81f4d4a00b3f3159ba
Fapse <fb-online@posteo.de>
2024-10-12 13:39:31 +0200

    Mandelbrot is now a class.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import mandelbrot
+from mandelbrot import Mandelbrot
+
 
 def show_window():
     window = tk.Tk()
-    ph = ImageTk.PhotoImage(mandelbrot.create_mandelbrot())
+    my_mandelbrot = Mandelbrot()
+    ph = ImageTk.PhotoImage(my_mandelbrot.create_mandelbrot())
     mandel_img = tk.Label(window, image=ph)
     mandel_img.image = ph
     greeting = tk.Label(text='Hello, Tkinter')
--- mandelbrot.py
+++ mandelbrot.py
@@ -7,30 +7,43 @@ from PIL import Image
 from PIL.ImageColor import getrgb
 from viewport import Viewport
 
-def hsb(hue_degrees: int, saturation: float, brightness: float):
-    return getrgb(
-        f"hsv({hue_degrees % 360},"
-        f"{saturation * 100}%,"
-        f"{brightness * 100}%)"
-    )
+class Mandelbrot:
 
-def create_mandelbrot() -> Image:
-    mandelbrot_set = MandelbrotSet(max_iterations=450, escape_radius=1000)
-    image = Image.new(mode="RGB", size=(512, 512))
-    #for pixel in Viewport(image, center=-0.75, width=3.5): # display complete mandelbrot set
-    for pixel in Viewport(image, center=-0.7435 + 0.1314j, width=0.002):
-        stability = mandelbrot_set.stability(complex(pixel), smooth=False)
-        pixel.color = (
-            (0, 0, 0)
-            if stability == 1
-            else hsb(
-                hue_degrees=int(stability * 360 + 80),
-                saturation=stability,
-                brightness=1,
-            )
+    def __init__(self):
+        self.center_real: float = -0.75
+        self.center_im: float = 0.0
+        self.width: float = 3.5
+        self.max_iterations: int = 25
+        self.escape_radius: int = 1000
+        self.image_width: int = 512
+        self.image_height: int = 512
+
+    @staticmethod
+    def hsb(hue_degrees: int, saturation: float, brightness: float) -> tuple[int, int, int] | tuple[int, int, int, int]:
+        return getrgb(
+            f"hsv({hue_degrees % 360},"
+            f"{saturation * 100}%,"
+            f"{brightness * 100}%)"
         )
-    return image
+
+    def create_mandelbrot(self) -> Image:
+        mandelbrot_set = MandelbrotSet(max_iterations=self.max_iterations, escape_radius=self.escape_radius)
+        image = Image.new(mode="RGB", size=(self.image_width, self.image_height))
+        #for pixel in Viewport(image, center=-0.75, width=3.5): # display complete mandelbrot set
+        for pixel in Viewport(image, center=complex(self.center_real, self.center_im), width=self.width):
+            stability = mandelbrot_set.stability(complex(pixel), smooth=True)
+            pixel.color = (
+                (0, 0, 0)
+                if stability == 1
+                else Mandelbrot.hsb(
+                    hue_degrees=int(stability * 360 + 80),
+                    saturation=stability,
+                    brightness=1,
+                )
+            )
+        return image
 
 if __name__ == "__main__":
+    my_mandelbrot = Mandelbrot()
     print("This might take a while...")
-    create_mandelbrot().show()
+    my_mandelbrot.create_mandelbrot().show()
