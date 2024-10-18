e741687e664d250a54cfd3b2f4d16f7fb51f53d7
Fapse <fb-online@posteo.de>
2024-10-18 20:49:10 +0200

    MandelbrotWindow add buttons.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ class MandelbrotWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("PyMandelbrot")
-        #self._cells = {}
         self._create_mandelbrot_display()
         self._create_mandelbrot_image()
         self._create_buttons()
@@ -32,18 +31,39 @@ class MandelbrotWindow(tk.Tk):
         display_frame.pack(side=tk.LEFT, expand=False)
         self.display.pack()
 
+    def _up_click(self):
+        print("Up clicked")
+
+    def _left_click(self):
+        print("Left clicked")
+
+    def _right_click(self):
+        print("Right clicked")
+
+    def _down_click(self):
+        print("Down clicked")
+
+    def _plus_click(self):
+        print("Plus clicked")
+
+    def _minus_click(self):
+        print("Minus clicked")
+
     def _create_buttons(self):
         display_frame = tk.Frame(master=self)
-        for x in range(3):
-            for y in range(3):
-                frame = tk.Frame(
-                    master=display_frame,
-                    relief=tk.RAISED,
-                    borderwidth=1
-                )
-                frame.grid(row=y, column=x, padx=3, pady=3)
-                label = tk.Label(master=frame, text=f"Row {y}\nColumn {x}")
-                label.pack()
+        button1=tk.Button(display_frame, text="U", command=self._up_click)
+        button1.grid(row=0, column=1)
+        button2=tk.Button(display_frame, text="L", command=self._left_click)
+        button2.grid(row=1, column=0)
+        button3=tk.Button(display_frame, text="R", command=self._right_click)
+        button3.grid(row=1, column=2)
+        button4=tk.Button(display_frame, text="D", command=self._down_click)
+        button4.grid(row=2, column=1)
+        button5=tk.Button(display_frame, text="-", command=self._minus_click)
+        button5.grid(row=3, column=0)
+        button6=tk.Button(display_frame, text="+", command=self._plus_click)
+        button6.grid(row=3, column=2)
+
         display_frame.pack(side=tk.LEFT, expand=False)
 
 if __name__ == "__main__":
